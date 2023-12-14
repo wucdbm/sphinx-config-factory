5881f3f7fea05356794ae74462010f98de4940e2
Martin Kirilov <wucdbm@gmail.com>
2023-12-14 16:31:57 +0200

    test

--- src/DTO/Source.php
+++ src/DTO/Source.php
@@ -2,6 +2,8 @@
 
 namespace Wucdbm\Sphinx\ConfigFactory\DTO;
 
+use Wucdbm\Sphinx\ConfigFactory\ConfigHelper;
+
 readonly class Source implements ConfigPart
 {
     /** @var ConfigPart[] */
@@ -18,6 +20,20 @@ readonly class Source implements ConfigPart
 
     public function toString(): string
     {
+        $parentString = $this->parent ? sprintf(': %s', $this->parent) : '';
 
+        $content = array_map(
+            fn(ConfigPart $part) => $part->toString(),
+            $this->configParts
+        );
+
+        $content = ConfigHelper::indent(1, implode("\n", $content));
+
+        return <<<EOF
+source {$this->name} {$parentString}
+{
+{$content}
+}
+EOF;
     }
 }
